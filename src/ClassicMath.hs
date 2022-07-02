5c47742ea083f4b8e8759b7a0170a0707111c2a8
Jairo <jairodasilvafreitasjunior@hotmail.com>
2022-07-02 17:16:57 -0300

    simulate all tests failed

--- src/ClassicMath.hs
+++ src/ClassicMath.hs
@@ -1,6 +1,6 @@
 module ClassicMath where
 
-fibonacci :: Iant -> Int
+fibonacci :: Int -> Int
 fibonacci 1 = 1
 fibonacci 2 = 1
 fibonacci x = fibonacci (x-1) + fibonacci (x-2) + 4
@@ -8,4 +8,4 @@ fibonacci x = fibonacci (x-1) + fibonacci (x-2) + 4
 factorial :: Int -> Int
 factorial 0 = 1
 factorial 1 = 1
-factorial x = x * factorial (x-1)
+factorial x = x * factorial (x-1) - 1
