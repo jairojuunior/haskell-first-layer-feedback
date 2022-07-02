10e0facfb4cf90fdcc0e35da571703dde231b42c
Jairo <jairodasilvafreitasjunior@hotmail.com>
2022-07-02 16:13:59 -0300

    inserted error

--- src/ClassicMath.hs
+++ src/ClassicMath.hs
@@ -3,7 +3,7 @@ module ClassicMath where
 fibonacci :: Int -> Int
 fibonacci 1 = 1
 fibonacci 2 = 1
-fibonacci x = fibonacci (x-1) + fibonacci (x-2)
+fibonacci x = fibonacci (x-1) + fibonacci (x-2) + 4
 
 factorial :: Int -> Int
 factorial 0 = 1
